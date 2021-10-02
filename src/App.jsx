4002adc6420c0db23380c8dc39e0d104ada90caf
devkiki7000 <devkiki7000@gmail.com>
2021-10-02 17:39:03 +0900

    feat(layouts): set navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import NavBar from "./layouts/NavBar";
+
 import Main from "./pages/Main";
 
 function App() {
     return (
         <div className="flex flex-col min-h-screen">
             <BrowserRouter>
+                <NavBar />
                 <div className="mb-auto flex-grow">
                     <Switch>
                         <Route exact path="/" component={Main} />
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class NavBar extends Component {
+    constructor (props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <>
+                <h3>This is navbar!</h3>
+            </>
+        )
+    }
+}
+
+export default NavBar;
