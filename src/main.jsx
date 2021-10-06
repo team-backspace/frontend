628164b2ef888b03f419d9199b1f97178908ab67
devkiki7000 <devkiki7000@gmail.com>
2021-10-06 10:53:16 +0900

    feat: use windicss

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "virtual:windi.css";
+import "antd/dist/antd.css";
+import "./styles/global.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 )
---
+++ windi.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "windicss/helpers";
+
+export default defineConfig({
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "color-lblue": "#0880AE",
+                "color-dblue": "#026f99"
+            }
+        }
+    },
+    plugins: []
+})
