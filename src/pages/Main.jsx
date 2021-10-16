ae833a6a4ce4c7d90a3e4c9df4ffffaa498d53b5
devkiki7000 <devkiki7000@gmail.com>
2021-10-17 01:00:03 +0900

    feat(pages.main): support 2xl size

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,7 +9,7 @@ class Main extends Component {
         return (        
             <>
                 <div className = "">
-                    <div className = "<xl:hidden px-50 grid grid-cols-2 h-screen flex items-center">
+                    <div className = "<xl:hidden px-50 2xl:px-80 grid grid-cols-2 h-screen flex items-center">
                         <div className = "-mt-30">
                             <h1 className = "text-4xl font-semibold text-purple-600">작품의 가치를 더하세요</h1>
                             <h4 className = "text-md font-semibold w-90">여러 주제의 작품을 둘러보고, 작품 업로드, 제작자에게 후원하기 등 다양한 작품과 소통하기 위한 기능들이 모두 준비되어 있습니다.</h4>
@@ -24,13 +24,13 @@ class Main extends Component {
                         </div>
                     </div>
                     <div className = "grid grid-cols-2 flex items-center">
-                        <img className = "-mt-40 ml-40" src = "src/dummydata/images/project-board.png" />
-                        <div className = "-mt-30 ml-30">
+                        <img className = "-mt-40 ml-40 2xl:ml-60" src = "src/dummydata/images/project-board.png" />
+                        <div className = "-mt-30 ml-30 2xl:ml-10">
                             <h3 className = "text-2xl font-medium">당신을 위한 작품을<br />직접 찾지 마세요</h3>
                             <h1 className = "text-4xl font-semibold">내 취향 작품들이 한눈에</h1>
                         </div>
                     </div>
-                    <div className = "2xl:px-40 xl:px-20 grid grid-cols-2 mt-20 flex items-center">
+                    <div className = "2xl:px-60 px-20 grid grid-cols-2 mt-20 flex items-center">
                         <div className = "-mt-30">
                             <h1 className = "text-4xl font-semibold">100% 익명성 커뮤니티</h1>
                             <div className = "">
@@ -56,7 +56,7 @@ class Main extends Component {
                         <img className = "-mt-10 ml-40" src = "src/dummydata/images/buy.png" />
                     </div>
                     <div className = "xl:w-9/12 flex <xl:flex-col items-center xl:(grid grid-cols-2) text-center">
-                        <img className = "w-60 xl:ml-70" src = "src/dummydata/images/start.png" />
+                        <img className = "w-60 xl:ml-70 2xl:ml-120" src = "src/dummydata/images/start.png" />
                         <div className = "mt-3 lg:mt-5 xl:mt-7">
                             <h3 className = "text-xl md:text-2xl xl:text-3xl font-semibold mb-2">다양한 작품들이 하나로</h3>
                             <h4 className = "text-sm md:text-base xl:text-lg"><i>Spacebook</i>은 대부분의 작품 유형을 지원합니다</h4>
