222f159bb1e45189df9ca067806592008731bacc
devkiki7000 <devkiki7000@gmail.com>
2021-10-18 02:46:13 +0900

    api(page.login): get login url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import { Button, Divider } from "antd";
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
@@ -22,6 +23,14 @@ class Login extends Component {
                     this.setState({ google_register_url: res })
                 })
         }
+        if (localStorage.google_login_url === undefined) {
+            api.get("/login", { application: "google_login" })
+                .then(res => res.data)
+                .then(res => {
+                    localStorage.google_login_url = res
+                    this.setState({ google_login_url: res })
+                })
+        }
     }
     render () {
         return (
@@ -30,18 +39,22 @@ class Login extends Component {
                     <h1 className = "text-3xl font-thin mb-2">로그인</h1>
                     <h4 className = "text-xs font-thin mb-6">다음으로 로그인</h4>
                     <div className = "">
-                        <Button className = "w-50" icon = {<GoogleOutlined />}>
-                            구글로 로그인
-                        </Button>
+                        <Link to = {this.state.google_login_url}>
+                            <Button className = "w-50" icon = {<GoogleOutlined />}>
+                                구글로 로그인
+                            </Button>
+                        </Link>
                     </div>
                 </div>
                 <div className = "flex-grow flex justify-center items-center flex-col h-auto md:h-100 <md:(mt-10 mb-20)">
                     <h1 className = "text-3xl font-thin mb-2">가입</h1>
                     <h4 className = "text-xs font-thin mb-6">다음으로 가입</h4>
                     <div className = "">
-                        <Button className = "w-50" icon = {<GoogleOutlined />}>
-                            구글로 가입
-                        </Button>
+                        <Link to = {this.state.google_register_url}>
+                            <Button className = "w-50" icon = {<GoogleOutlined />}>
+                                구글로 가입
+                            </Button>
+                        </Link>
                     </div>
                 </div>
             </div>
