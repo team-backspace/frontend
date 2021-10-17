ba8e929390e7ce8446666286c5a9792dbd56d758
devkiki7000 <devkiki7000@gmail.com>
2021-10-18 02:31:02 +0900

    feat(pages.project): support responsive

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,12 +8,12 @@ class Project extends Component {
     render () {
         return (
             <>
-                <div className = "px-30 pt-10 grid grid-cols-2">
+                <div className = "px-30 pt-10 grid lg:grid-cols-2">
                     <div className = "">
-                        <img className = "w-150 h-80 rounded-2xl" src = "../src/dummydata/images/CardImage.png" />
+                        <img className = "w-full lg:w-150 h-80 rounded-2xl" src = "../src/dummydata/images/CardImage.png" />
                     </div>
                     <div className = "ml-10">
-                        <h2 className = "text-3xl font-thin">가을의 섬</h2>
+                        <h2 className = "text-3xl font-thin <lg:mt-10">가을의 섬</h2>
                         <div className = "flex">
                             <Avatar src = "../src/dummydata/images/patrick.png" />
                             <h4 className = "text-base font-thin ml-3 mt-1">PatrickKR</h4>
